9e74d55830cb40b593e0d1210e3dd420bc2c3654
Kevin Mehall <km@kevinmehall.net>
2013-03-06 14:28:42 -0700

    Remove unnecessary headers

--- src/node_usb.h
+++ src/node_usb.h
@@ -1,23 +1,13 @@
 #ifndef SRC_NODE_USB_H
 #define SRC_NODE_USB_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
 #include <assert.h>
-#include <iostream>
-
-#include <cstring>
 #include <string>
-#include <cstdlib>
 
 #include <libusb-1.0/libusb.h>
 #include <v8.h>
 
 #include <node.h>
-#include <node_version.h>
 #include <node_buffer.h>
 #include <uv.h>
 
