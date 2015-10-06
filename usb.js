6f18d66e081b25ccb4a00feec1ecc755690725b3
flames of love <mechanicofthesequence@gmail.com>
2015-10-06 22:42:16 +0200

    setConfiguration option

--- usb.js
+++ usb.js
@@ -29,8 +29,9 @@ exports.findByIds = function(vid, pid) {
 
 usb.Device.prototype.timeout = 1000
 
-usb.Device.prototype.open = function(){
+usb.Device.prototype.open = function(defaultConfig){
 	this.__open()
+	if (defaultConfig === false) return
 	this.interfaces = []
 	var len = this.configDescriptor.interfaces.length
 	for (var i=0; i<len; i++){
@@ -128,6 +129,20 @@ usb.Device.prototype.getStringDescriptor = function (desc_index, callback) {
 			callback(undefined, buf.toString('utf16le', 2));
 		}
 	);
+}
+
+usb.Device.prototype.setConfiguration = function(desired, cb) {
+	var self = this;
+	this.__setConfiguration(desired, function(err) {
+		if (!err) {
+			this.interfaces = []
+			var len = this.configDescriptor.interfaces.length
+			for (var i=0; i<len; i++) {
+				this.interfaces[i] = new Interface(this, i)
+			}
+		}
+		cb.call(self, err)
+	});
 }
 
 function Interface(device, id){
