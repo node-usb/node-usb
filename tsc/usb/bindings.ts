d8906906e8c649be3db3ffed64e0d00c05f16423
thegecko <rob.moran@arm.com>
2021-10-14 21:56:57 +0100

    Updates

--- tsc/usb/bindings.ts
+++ tsc/usb/bindings.ts
@@ -5,7 +5,7 @@
 import type { DeviceDescriptor, ConfigDescriptor, BosDescriptor } from './descriptors';
 
 /* eslint-disable @typescript-eslint/no-var-requires */
-const usb = require('bindings')('usb_bindings');
+const usb = require('node-gyp-build')(__dirname);
 module.exports = usb;
 
 /**
