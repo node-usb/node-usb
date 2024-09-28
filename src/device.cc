235fd7db787e77d72fbdad951636535ff6e5bab9
thegecko <github@thegecko.org>
2024-09-28 19:09:17 +0100

    dont use cache

--- src/device.cc
+++ src/device.cc
@@ -37,17 +37,6 @@ Device::~Device() {
 // Get a V8 instance for a libusb_device: either the existing one from the map,
 // or create a new one and add it to the map.
 Napi::Object Device::get(Napi::Env env, libusb_device* dev) {
-    ModuleData* instanceData = env.GetInstanceData<ModuleData>();
-    std::map<libusb_device*, Device*>& byPtr = instanceData->byPtr;
-
-    auto it = byPtr.find(dev);
-    if (it != byPtr.end()) {
-        auto value = it->second->Value();
-        // JS object may have already been garbage collected
-        if (!value.IsEmpty())
-            return value;
-    }
-
     Napi::Object obj = instanceData->deviceConstructor.New({ Napi::External<libusb_device>::New(env, dev) });
     return obj;
 }
